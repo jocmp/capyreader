e51608d4d7196af4124b3e64b431adf3bd2db243
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2023-05-21 17:09:26 -0500

    bump version 2023.05.1010-beta -> 2023.05.1011

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.jocmp.basilreader"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1010
-        versionName = "2023.05.1010-beta"
+        versionCode = 1011
+        versionName = "2023.05.1011"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
