2a49a6e5757a844bc7dbd5c7884ace823fe24a25
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2023-12-10 18:23:11 -0600

    Call Feed ID by title (for now)

--- basil/src/main/java/com/jocmp/basil/extensions/FeedOutlineExt.kt
+++ basil/src/main/java/com/jocmp/basil/extensions/FeedOutlineExt.kt
@@ -6,7 +6,7 @@ import com.jocmp.basil.opml.Outline
 internal val Outline.FeedOutline.asFeed: Feed
     get() {
         return Feed(
-            id = "",
+            id = feed.title.toString(),
             name = feed.title ?: "",
             feedURL = feed.xmlUrl ?: ""
         )
