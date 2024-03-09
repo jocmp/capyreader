37264f38d7ddac503b100829d0dfa5ae7428af40
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-03-09 15:42:23 -0600

    Make nav bar match background

--- app/src/main/java/com/jocmp/basilreader/ui/theme/Theme.kt
+++ app/src/main/java/com/jocmp/basilreader/ui/theme/Theme.kt
@@ -58,6 +58,7 @@ fun CapyTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
