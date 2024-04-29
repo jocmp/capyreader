3725d24779169817df905a702ad36842f763671b
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-04-29 12:04:09 -0500

    Offset placeholder height by topbar size

--- app/src/main/java/com/jocmp/basilreader/ui/articles/detail/ArticleView.kt
+++ app/src/main/java/com/jocmp/basilreader/ui/articles/detail/ArticleView.kt
@@ -1,27 +1,20 @@
 package com.jocmp.basilreader.ui.articles.detail
 
-import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.jocmp.basil.Article
-import com.jocmp.basilreader.R
-import com.jocmp.basilreader.ui.components.EmptyView
-import com.jocmp.basilreader.ui.components.LoadingState
-import com.jocmp.basilreader.ui.components.WebContent
 import com.jocmp.basilreader.ui.components.WebView
 import com.jocmp.basilreader.ui.components.WebViewNavigator
-import com.jocmp.basilreader.ui.components.WebViewState
 import com.jocmp.basilreader.ui.components.rememberSaveableWebViewState
 
 @Composable
@@ -54,12 +47,11 @@ fun ArticleView(
             if (article == null) {
                 Box(
                     contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .padding(bottom = 64.dp)
+                        .fillMaxSize()
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                        contentDescription = null,
-                    )
+                    CapyPlaceholder()
                 }
             }
 
