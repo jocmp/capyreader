db340d66321cbaed684d0c54f0e49df21aa99454
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-07-07 16:42:30 -0500

    Constrain max height on tags

--- app/src/main/java/com/capyreader/app/ui/articles/EditFeedDialog.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/EditFeedDialog.kt
@@ -1,11 +1,9 @@
 package com.capyreader.app.ui.articles
 
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.jocmp.capy.EditFeedFormEntry
 import com.jocmp.capy.Feed
