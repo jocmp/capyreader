a6b4d71b84c2eb1f1605ca5a02fa3d4639577a27
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-07-07 15:49:52 -0500

    Replace references of folders with tags

--- app/src/main/java/com/capyreader/app/ui/articles/EditFeedView.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/EditFeedView.kt
@@ -9,7 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -71,8 +75,7 @@ fun EditFeedView(
             .verticalScroll(scrollState)
             .padding(16.dp)
     ) {
-
-        TextField(
+        OutlinedTextField(
             value = name,
             onValueChange = setName,
             placeholder = { Text(feed.title) },
@@ -85,10 +88,10 @@ fun EditFeedView(
             ),
             modifier = Modifier.fillMaxWidth()
         )
-        TextField(
+        OutlinedTextField(
             value = addedFolder,
             onValueChange = setAddedFolder,
-            placeholder = {
+            label = {
                 Text(stringResource(id = R.string.add_feed_new_folder_title))
             },
             keyboardOptions = KeyboardOptions(
