579a3b5d37a0c06d057b9371e6d62e8e8c7ab229
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-07-27 14:04:38 -0500

    Add custom extractor and styling for factorio

--- capy/src/main/assets/custom-extractors.js
+++ capy/src/main/assets/custom-extractors.js
@@ -1,23 +1,42 @@
-const tagesschauExtractor = {
-  domain: 'www.tagesschau.de',
-  title: {
-    selectors: ['.seitenkopf__headline'],
-  },
-  author: {
-    selectors: ['.authorline__author'],
-  },
-  content: {
-    selectors: ['article'],
-    clean: [
-      '[data-config]',
-      '.seitenkopf__headline',
-      '.authorline__author',
-      '.metatextline'
-    ],
-  },
-  date_published: {
-    selectors: ['.metatextline'],
-  },
-};
+/** @param {Record<string, any>[]} list */
+function extractors(list) {
+  list.forEach(extractor => Mercury.addExtractor(extractor));
+}
 
-Mercury.addExtractor(tagesschauExtractor)
+extractors([
+  {
+    domain: 'www.tagesschau.de',
+    title: {
+      selectors: ['.seitenkopf__headline'],
+    },
+    author: {
+      selectors: ['.authorline__author'],
+    },
+    content: {
+      selectors: ['article'],
+      clean: [
+        '[data-config]',
+        '.seitenkopf__headline',
+        '.authorline__author',
+        '.metatextline'
+      ],
+    },
+    date_published: {
+      selectors: ['.metatextline'],
+    },
+  },
+  {
+    domain: 'factorio.com',
+    content: {
+      selectors: [
+        ['.blog-post', 'div:nth-child(2)']
+      ],
+      transforms: {
+        'h3 author': (node) => {
+          node.attr('class', 'article__body--subheading')
+          return 'span'
+        },
+      }
+    }
+  },
+]);
