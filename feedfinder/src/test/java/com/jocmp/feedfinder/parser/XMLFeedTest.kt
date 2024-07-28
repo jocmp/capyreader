11ca06a807022ed7f0ace85a9312abf6257ce849
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-07-28 18:25:37 -0500

    Catch errors on site URLs when parsing feed

--- feedfinder/src/test/java/com/jocmp/feedfinder/parser/XMLFeedTest.kt
+++ feedfinder/src/test/java/com/jocmp/feedfinder/parser/XMLFeedTest.kt
@@ -1,18 +1,33 @@
 package com.jocmp.feedfinder.parser
 
 import kotlinx.coroutines.runBlocking
-import org.junit.Test
+import kotlinx.coroutines.test.runTest
 import java.io.File
 import java.net.URL
+import kotlin.test.Test
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class XMLFeedTest {
     @Test
-    fun isValid() = runBlocking {
+    fun isValid() = runTest {
         val responseBody = File("src/test/resources/arstechnica_feed.xml").readText()
 
         val feed = XMLFeed.from(url = URL("https://arstechnica.com"), body = responseBody)
 
         assertTrue(feed.isValid())
+    }
+
+    @Test
+    fun siteURLIsInvalid() = runTest {
+        val responseBody = File("src/test/resources/brasildefato_com_br.xml").readText()
+
+        val feed = XMLFeed.from(
+            url = URL("https://www.brasildefato.com.br/rss2.xml"),
+            body = responseBody
+        )
+
+        assertTrue(feed.isValid())
+        assertNull(feed.siteURL)
     }
 }
