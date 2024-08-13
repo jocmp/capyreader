a7148ec9a60792447310dc9bb2ea079765a01670
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-08-13 17:23:40 -0500

    Add 12 and 24 hour refresh intervals

--- app/src/main/java/com/capyreader/app/ui/settings/RefreshIntervalMenu.kt
+++ app/src/main/java/com/capyreader/app/ui/settings/RefreshIntervalMenu.kt
@@ -70,10 +70,12 @@ fun RefreshIntervalMenu(
 private fun Context.translationKey(refreshInterval: RefreshInterval): String {
     return when (refreshInterval) {
         MANUALLY_ONLY -> getString(R.string.refresh_manually_only)
+        ON_START -> getString(R.string.refresh_on_start)
         EVERY_FIFTEEN_MINUTES -> getString(R.string.refresh_minutes, 15)
         EVERY_THIRTY_MINUTES -> getString(R.string.refresh_minutes, 30)
-        EVERY_HOUR -> resources.getQuantityString(R.plurals.refresh_hours, 1)
-        ON_START -> getString(R.string.refresh_on_start)
+        EVERY_HOUR -> resources.getQuantityString(R.plurals.refresh_hours, 1, 1)
+        EVERY_12_HOURS -> resources.getQuantityString(R.plurals.refresh_hours, 12, 12)
+        EVERY_DAY -> getString(R.string.refresh_every_day)
     }
 }
 
