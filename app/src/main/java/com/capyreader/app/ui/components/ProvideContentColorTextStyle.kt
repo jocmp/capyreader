b6d4c6e3c6f1106ad35a39eba29787e98f2735f2
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-09-09 22:37:04 -0500

    Write custom artice list row

---
+++ app/src/main/java/com/capyreader/app/ui/components/ProvideContentColorTextStyle.kt
@@ -0,0 +1,22 @@
+package com.capyreader.app.ui.components
+
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun ProvideContentColorTextStyle(
+    contentColor: Color,
+    textStyle: TextStyle,
+    content: @Composable () -> Unit
+) {
+    val mergedStyle = LocalTextStyle.current.merge(textStyle)
+    CompositionLocalProvider(
+        LocalContentColor provides contentColor,
+        LocalTextStyle provides mergedStyle,
+        content = content
+    )
+}
