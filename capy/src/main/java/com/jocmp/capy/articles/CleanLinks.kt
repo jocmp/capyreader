cc72daa5b52c7b32d065fe670fca12171fa158dc
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-09-22 23:01:05 -0500

    Remove comments

--- capy/src/main/java/com/jocmp/capy/articles/CleanLinks.kt
+++ capy/src/main/java/com/jocmp/capy/articles/CleanLinks.kt
@@ -3,8 +3,12 @@ package com.jocmp.capy.articles
 import org.jsoup.nodes.Element
 
 internal fun cleanLinks(element: Element) {
-    element.getElementsByTag("img").forEach { child ->
+    element.getElementsByTag("img").forEachIndexed{ index, child ->
         child.attr("src", child.absUrl("src"))
+
+        if (index > 0) {
+            child.attr("loading", "lazy")
+        }
     }
 
     element.select("img[data-src]").forEach { child ->
