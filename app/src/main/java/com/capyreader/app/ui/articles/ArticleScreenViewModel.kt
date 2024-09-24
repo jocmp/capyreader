f69983744a09fd1424df6d3336189358e57b733d
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-09-23 19:28:36 -0500

    Set initialized after refresh

--- app/src/main/java/com/capyreader/app/ui/articles/ArticleScreenViewModel.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/ArticleScreenViewModel.kt
@@ -32,6 +32,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import java.time.OffsetDateTime
 
