a484f0a54082e948f61cce3421d0af98f6b368d4
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-09-29 16:14:12 -0500

    Add article list scrollbar (#378)

---
+++ app/src/main/java/com/capyreader/app/ui/articles/ColumnScrollbar.kt
@@ -0,0 +1,39 @@
+package com.capyreader.app.ui.articles
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import my.nanihadesuka.compose.ColumnScrollbar
+import my.nanihadesuka.compose.LazyColumnScrollbar
+import my.nanihadesuka.compose.ScrollbarSettings
+
+@Composable
+fun ColumnScrollbar(state: ScrollState, content: @Composable () -> Unit) {
+    ColumnScrollbar(
+        state = state,
+        settings = settings()
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun LazyScrollbar(state: LazyListState, content: @Composable () -> Unit) {
+    LazyColumnScrollbar(
+        state = state,
+        settings = settings()
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun settings() = ScrollbarSettings.Default.copy(
+    thumbThickness = 4.dp,
+    hideDisplacement = 0.dp,
+    scrollbarPadding = 2.dp,
+    thumbSelectedColor = colorScheme.onSurfaceVariant,
+    thumbUnselectedColor = colorScheme.onSurfaceVariant
+)
--- app/src/main/java/com/capyreader/app/ui/articles/detail/ArticleReader.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/detail/ArticleReader.kt
@@ -6,27 +6,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.capyreader.app.ui.articles.Scrollbar
+import com.capyreader.app.ui.articles.ColumnScrollbar
 import com.capyreader.app.ui.components.WebView
 import com.capyreader.app.ui.components.rememberWebViewState
 import com.jocmp.capy.Article
-import com.jocmp.capy.articles.ArticleRenderer
 import kotlinx.coroutines.launch
-import my.nanihadesuka.compose.ColumnScrollbar
-import my.nanihadesuka.compose.ScrollbarSettings
-import org.koin.compose.koinInject
 
 @Composable
 fun ArticleReader(
@@ -38,7 +31,7 @@ fun ArticleReader(
     var lastScrollY by rememberSaveable { mutableIntStateOf(0) }
     val webViewState = rememberWebViewState()
 
-    Scrollbar(scrollState = scrollState) {
+    ColumnScrollbar(state = scrollState) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
