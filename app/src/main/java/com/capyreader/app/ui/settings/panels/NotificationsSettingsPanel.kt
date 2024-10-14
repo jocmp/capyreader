48954fb188e0a4f5422fc99d24fb2bd0dbdbe20d
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-10-13 23:26:13 -0500

    Add spacing at bottom of notifications list

--- app/src/main/java/com/capyreader/app/ui/settings/panels/NotificationsSettingsPanel.kt
+++ app/src/main/java/com/capyreader/app/ui/settings/panels/NotificationsSettingsPanel.kt
@@ -1,11 +1,15 @@
 package com.capyreader.app.ui.settings.panels
 
+import android.widget.Space
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.capyreader.app.ui.fixtures.FeedSample
 import com.capyreader.app.ui.theme.CapyTheme
 import com.jocmp.capy.Feed
@@ -33,6 +37,7 @@ fun NotificationsSettingsPanel(
                 feed = feed
             )
         }
+        Spacer(Modifier.height(16.dp))
     }
 }
 
