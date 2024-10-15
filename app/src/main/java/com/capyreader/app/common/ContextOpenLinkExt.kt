c8961baa9720b881b52c2171f8df323142bb6165
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-10-14 20:31:20 -0500

    Add swipe option to open links in browser (#428)

--- app/src/main/java/com/capyreader/app/common/ContextOpenLinkExt.kt
+++ app/src/main/java/com/capyreader/app/common/ContextOpenLinkExt.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
+import java.net.URL
 
 fun Context.openLink(url: Uri, appPreferences: AppPreferences? = null) {
     if (appPreferences != null && appPreferences.openLinksInternally.get()) {
@@ -13,7 +14,15 @@ fun Context.openLink(url: Uri, appPreferences: AppPreferences? = null) {
 
         intent.launchUrl(this, url)
     } else {
-        val intent = Intent(Intent.ACTION_VIEW, url)
-        startActivity(intent)
+        openLinkExternally(url)
     }
+}
+
+fun Context.openLinkExternally(url: URL) {
+    openLinkExternally(Uri.parse(url.toString()))
+}
+
+private fun Context.openLinkExternally(url: Uri) {
+    val intent = Intent(Intent.ACTION_VIEW, url)
+    startActivity(intent)
 }
--- app/src/main/java/com/capyreader/app/ui/settings/panels/RowSwipeOption.kt
+++ app/src/main/java/com/capyreader/app/ui/settings/panels/RowSwipeOption.kt
@@ -5,13 +5,15 @@ import com.capyreader.app.R
 enum class RowSwipeOption {
     DISABLED,
     TOGGLE_READ,
-    TOGGLE_STARRED;
+    TOGGLE_STARRED,
+    OPEN_EXTERNALLY;
 
     val translationKey: Int
         get() = when (this) {
             DISABLED -> R.string.article_list_row_swipe_disabled
             TOGGLE_READ -> R.string.article_list_row_swipe_toggle_read
             TOGGLE_STARRED -> R.string.article_list_row_swipe_toggle_starred
+            OPEN_EXTERNALLY -> R.string.article_list_row_swipe_open_externally
         }
 
     companion object {
@@ -22,6 +24,7 @@ enum class RowSwipeOption {
                 DISABLED,
                 TOGGLE_READ,
                 TOGGLE_STARRED,
+                OPEN_EXTERNALLY,
             )
     }
 }
