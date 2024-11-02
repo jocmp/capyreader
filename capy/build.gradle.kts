89807b8e989179913e71bd906f9b885b282d47b2
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-11-02 17:40:53 -0500

    Add missing proguard mappings

--- capy/build.gradle.kts
+++ capy/build.gradle.kts
@@ -32,6 +32,8 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
