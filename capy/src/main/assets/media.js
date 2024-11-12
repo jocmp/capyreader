c3809b7309a51a504b4a5f14de1e6bdf114ef49a
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-11-11 20:37:55 -0600

    Clean up 'style' tags

--- capy/src/main/assets/media.js
+++ capy/src/main/assets/media.js
@@ -12,39 +12,17 @@ function configureVideoTags() {
   });
 }
 
-function cleanAnchorImageTags() {
-  [...document.querySelectorAll("a img")].forEach((img) => {
-    attachImageToAnchorParent(img, img.parentNode);
-  });
-}
-
-/**
- *
- * @param {HTMLImageElement} img
- * @param {ParentNode | null} parentNode
- */
-function attachImageToAnchorParent(img, parentNode) {
-  if (parentNode == null || parentNode.tagName === "DOCUMENT") {
-    return;
-  } else if (parentNode.tagName === "A") {
-    let anchor = parentNode;
-    anchor.parentNode?.appendChild(img);
-    anchor.parentNode?.removeChild(anchor);
-  } else {
-    attachImageToAnchorParent(img, parentNode.parentNode);
-  }
-}
-
 function addImageClickListeners() {
-  [...document.getElementsByTagName("img")].forEach((img) => {
-    img.addEventListener("click", () => {
-      Android.openImage(img.src);
+  [...document.querySelectorAll("img")]
+    .filter(i => !i.parentNode.closest("a"))
+    .forEach((img) => {
+      img.addEventListener("click", () => {
+        Android.openImage(img.src);
+      });
     });
-  });
 }
 
 window.onload = () => {
-  cleanAnchorImageTags();
   addImageClickListeners();
   configureVideoTags();
 };
---
+++ capy/src/main/java/com/jocmp/capy/articles/CleanStyles.kt
@@ -0,0 +1,13 @@
+package com.jocmp.capy.articles
+
+import org.jsoup.nodes.Document
+
+fun cleanStyles(document: Document) {
+    document.getElementsByTag("style").forEach {
+        it.remove()
+    }
+
+    document.select("#article-body-content *").forEach {
+        it.removeAttr("style")
+    }
+}
