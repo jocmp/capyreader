15dee8711d89416cb387aa302930ddb591809f31
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-11-18 21:05:31 -0600

    Exclude gravatar from article preview

---
+++ rssparser/src/main/kotlin/com/prof18/rssparser/internal/ImagePolicy.kt
@@ -0,0 +1,12 @@
+package com.prof18.rssparser.internal
+
+internal object ImagePolicy {
+    fun isValidArticleImage(src: String): Boolean {
+        return src.isNotBlank() &&
+                !src.contains(EMOJI_DOMAIN) &&
+                !src.contains(GRAVATAR_DOMAIN)
+    }
+
+    private const val EMOJI_DOMAIN = "s.w.org/images/core/emoji"
+    private const val GRAVATAR_DOMAIN = "gravatar.com"
+}
