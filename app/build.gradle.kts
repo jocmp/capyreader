13ee1d357abf06533efbec59413f2fa860d9bacb
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-01-21 17:14:58 -0600

    Add support for SVG favicons (#759)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -129,6 +129,7 @@ dependencies {
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.coil.compose)
     implementation(libs.coil.gif)
+    implementation(libs.coil.svg)
     implementation(libs.lazycolumnscrollbar)
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
