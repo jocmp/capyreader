fd7c37ee6df06189aaf750504070dd5af52739b3
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-02-16 15:15:26 -0600

    Add 'Mark as read' button to notifications

--- app/src/main/java/com/capyreader/app/ArticleStatusBroadcastReceiver.kt
+++ app/src/main/java/com/capyreader/app/ArticleStatusBroadcastReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.capyreader.app.notifications.NotificationHelper
+import com.capyreader.app.sync.Sync
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -11,10 +12,16 @@ class ArticleStatusBroadcastReceiver : BroadcastReceiver(), KoinComponent {
     private val handler: BroadcastHandler by lazy { BroadcastHandler() }
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == ACTION_DISMISS_NOTIFICATION) {
-            val articleID = intent.getStringExtra(ARTICLE_ID) ?: return
+        val articleID = intent.getStringExtra(ARTICLE_ID) ?: return
 
-            handler.dismissNotification(articleID)
+        when (intent.action) {
+            ACTION_DISMISS_NOTIFICATION ->
+                handler.dismissNotification(articleID)
+
+            ACTION_MARK_AS_READ -> {
+                handler.markAsRead(articleID, context = context)
+                handler.dismissNotification(articleID)
+            }
         }
     }
 
@@ -24,11 +31,17 @@ class ArticleStatusBroadcastReceiver : BroadcastReceiver(), KoinComponent {
         fun dismissNotification(articleID: String) {
             notificationHelper.dismissNotifications(listOf(articleID))
         }
+
+        fun markAsRead(articleID: String, context: Context) {
+            Sync.markReadAsync(listOf(articleID), context)
+        }
     }
 
     companion object {
-        const val ACTION_DISMISS_NOTIFICATION = "com.capyreader.ACTION_DISMISS_NOTIFICATION"
+        const val ACTION_DISMISS_NOTIFICATION = "com.capyreader.app.ACTION_DISMISS_NOTIFICATION"
 
-        const val ARTICLE_ID = "com.capyreader.articles.article_id"
+        const val ACTION_MARK_AS_READ = "com.capyreader.app.MARK_AS_READ"
+
+        const val ARTICLE_ID = "com.capyreader.app.articles.article_id"
     }
 }
