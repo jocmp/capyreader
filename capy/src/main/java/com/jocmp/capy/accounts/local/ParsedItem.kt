539237feeaa38af9fd97d1cbbe322ed511046da4
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-02-15 18:50:49 -0600

    [Local] Handle empty guids (#867)

--- capy/src/main/java/com/jocmp/capy/accounts/local/ParsedItem.kt
+++ capy/src/main/java/com/jocmp/capy/accounts/local/ParsedItem.kt
@@ -10,7 +10,7 @@ import java.net.URL
 internal class ParsedItem(private val item: RssItem, private val siteURL: String?) {
     val url: String? = articleURL()
 
-    val id: String? = item.guid ?: url
+    val id: String? = item.guid.orEmpty().ifBlank { url?.ifBlank { null } }
 
     val contentHTML: String?
         get() {
