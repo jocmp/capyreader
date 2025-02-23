94f586c8bbf8b2a2fcbffe1f62cab06164aae14c
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-02-23 17:49:00 -0500

    Fix padding on onboarding screen

--- app/src/main/java/com/capyreader/app/ui/articles/list/EmptyOnboardingView.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/list/EmptyOnboardingView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.capyreader.app.R
@@ -23,6 +25,7 @@ fun EmptyOnboardingView(
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
+            .padding(horizontal = 16.dp)
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -30,18 +33,34 @@ fun EmptyOnboardingView(
         ) {
             Text(
                 stringResource(R.string.empty_onboarding_title),
-                color = colorScheme.secondary
+                color = colorScheme.secondary,
+                textAlign = TextAlign.Center,
             )
             Text(
                 stringResource(R.string.empty_onboarding_prompt),
-                color = colorScheme.secondary
+                color = colorScheme.secondary,
+                textAlign = TextAlign.Center,
             )
             addFeed()
         }
     }
 }
 
-@Preview
+@Preview(locale = "de", device = "spec:width=960px,height=2340px,dpi=440")
+@Composable
+private fun EmptyOnboardingViewPreview_DE() {
+    CapyTheme {
+        EmptyOnboardingView(
+            addFeed = {
+                OutlinedButton(onClick = { }) {
+                    Text("+ Add Feed")
+                }
+            }
+        )
+    }
+}
+
+@Preview(locale = "bg", device = "spec:width=960px,height=2340px,dpi=440")
 @Composable
 private fun EmptyOnboardingViewPreview() {
     CapyTheme {
