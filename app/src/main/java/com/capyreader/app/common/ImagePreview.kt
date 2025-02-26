c83a6e6eeed1b49efc15aa5b4df18e97c0639b79
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-02-25 21:30:39 -0600

    Add medium preview image option (#906)

--- app/src/main/java/com/capyreader/app/common/ImagePreview.kt
+++ app/src/main/java/com/capyreader/app/common/ImagePreview.kt
@@ -5,14 +5,20 @@ import com.capyreader.app.R
 enum class ImagePreview {
     NONE,
     SMALL,
+    MEDIUM,
     LARGE;
 
     val translationKey: Int
         get() = when (this) {
             NONE -> R.string.image_preview_menu_option_none
             SMALL -> R.string.image_preview_menu_option_small
+            MEDIUM -> R.string.image_preview_menu_option_medium
             LARGE -> R.string.image_preview_menu_option_large
         }
+
+    fun showInline(): Boolean {
+        return this == SMALL || this == MEDIUM
+    }
 
     companion object {
         val default = SMALL
@@ -21,6 +27,7 @@ enum class ImagePreview {
             get() = listOf(
                 NONE,
                 SMALL,
+                MEDIUM,
                 LARGE,
             )
     }
