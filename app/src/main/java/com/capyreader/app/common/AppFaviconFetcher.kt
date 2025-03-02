16c836d70aa7d1cb28810b2ac2fa35a099fc5bbf
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-03-02 17:57:33 -0600

    wip

--- app/src/main/java/com/capyreader/app/common/AppFaviconFetcher.kt
+++ app/src/main/java/com/capyreader/app/common/AppFaviconFetcher.kt
@@ -3,7 +3,10 @@ package com.capyreader.app.common
 import android.content.Context
 import coil.imageLoader
 import coil.request.ImageRequest
+import com.jocmp.capy.Feed
 import com.jocmp.capy.accounts.FaviconFetcher
+import java.net.MalformedURLException
+import java.net.URI
 
 class AppFaviconFetcher(private val context: Context) : FaviconFetcher {
     override suspend fun isValid(url: String?): Boolean {
@@ -17,5 +20,21 @@ class AppFaviconFetcher(private val context: Context) : FaviconFetcher {
             )
 
         return result.drawable != null
+    }
+
+    override suspend fun findFaviconURL(feed: Feed): String? {
+        val baseURL = feed.siteURL.ifBlank { feed.feedURL }
+
+        return try {
+            val faviconURL = URI(baseURL).resolve("favicon.ico").toString()
+
+            if (isValid(faviconURL)) {
+                return faviconURL
+            }
+
+            return null
+        } catch (e: MalformedURLException) {
+            null
+        }
     }
 }
