5c95cdaad3964d3b7366ae2dad645ac1c9eef73a
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-03-02 17:57:33 -0600

    wip

--- capy/src/main/java/com/jocmp/capy/accounts/FaviconFetcher.kt
+++ capy/src/main/java/com/jocmp/capy/accounts/FaviconFetcher.kt
@@ -1,5 +1,9 @@
 package com.jocmp.capy.accounts
 
+import com.jocmp.capy.Feed
+
 interface FaviconFetcher {
     suspend fun isValid(url: String?): Boolean
+
+    suspend fun findFaviconURL(feed: Feed): String?
 }
