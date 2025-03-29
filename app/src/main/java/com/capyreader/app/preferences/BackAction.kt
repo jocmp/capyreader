f38e20b8a5ba6d5b6d4548e2f5ff95f4ac609170
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-03-29 14:57:42 -0500

    Add 'Navigate to Parent' back nav option

--- app/src/main/java/com/capyreader/app/preferences/BackAction.kt
+++ app/src/main/java/com/capyreader/app/preferences/BackAction.kt
@@ -4,12 +4,14 @@ import com.capyreader.app.R
 
 enum class BackAction {
     SYSTEM_BACK,
-    OPEN_DRAWER;
+    OPEN_DRAWER,
+    NAVIGATE_TO_PARENT;
 
     val translationKey: Int
         get() = when (this) {
             SYSTEM_BACK -> R.string.settings_gestures_list_back_navigation_system_back
             OPEN_DRAWER -> R.string.settings_gestures_list_back_navigation_open_drawer
+            NAVIGATE_TO_PARENT -> R.string.settings_gestures_list_back_navigation_navigate_to_parent
         }
 
     companion object {
