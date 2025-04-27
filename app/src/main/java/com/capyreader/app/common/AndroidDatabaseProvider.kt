fc2b3600ce13fd3e9d675b11ca644380e5cecef1
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-04-27 12:49:13 -0500

    Enable Write-Ahead Logging for database

--- app/src/main/java/com/capyreader/app/common/AndroidDatabaseProvider.kt
+++ app/src/main/java/com/capyreader/app/common/AndroidDatabaseProvider.kt
@@ -1,10 +1,11 @@
 package com.capyreader.app.common
 
 import android.content.Context
-import android.util.Log
+import androidx.sqlite.db.SupportSQLiteDatabase
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.jocmp.capy.DatabaseProvider
 import com.jocmp.capy.db.Database
+import com.jocmp.capy.logging.CapyLog
 
 class AndroidDatabaseProvider(private val context: Context) : DatabaseProvider {
     override fun build(accountID: String): Database {
@@ -12,7 +13,25 @@ class AndroidDatabaseProvider(private val context: Context) : DatabaseProvider {
             Database.Schema,
             context,
             databaseName(accountID),
-            windowSizeBytes = 100 * 1024 * 1024
+            windowSizeBytes = 100 * 1024 * 1024,
+            /**
+             * - https://developer.android.com/topic/performance/sqlite-performance-best-practices#enable-write-ahead
+             */
+            callback = object : AndroidSqliteDriver.Callback(Database.Schema) {
+                override fun onConfigure(db: SupportSQLiteDatabase) {
+                    super.onConfigure(db)
+                    tryEnableWriteAheadLogging(db)
+                }
+
+                private fun tryEnableWriteAheadLogging(db: SupportSQLiteDatabase) {
+                    try {
+                        db.enableWriteAheadLogging()
+                        db.execSQL("PRAGMA synchronous = NORMAL")
+                    } catch (e: Exception) {
+                        CapyLog.error("androiddb", e)
+                    }
+                }
+            }
         )
 
         return Database(driver)
