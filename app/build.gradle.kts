6d10c3e807bee826389f95a14ddc938df8de7e05
jocmp <9521010+jocmp@users.noreply.github.com>
2025-05-29 23:20:13 +0000

    Bump version 2025.05.1137-dev to 2025.05.1138

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.capyreader.app"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1137
-        versionName = "2025.05.1137-dev"
+        versionCode = 1138
+        versionName = "2025.05.1138"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
