d6f126e36aa2f2691d67c0493726d0527d864983
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-06-03 19:57:56 -0500

    Fix feed list sort for unicode characters

--- capy/src/main/java/com/jocmp/capy/common/FeedListExt.kt
+++ capy/src/main/java/com/jocmp/capy/common/FeedListExt.kt
@@ -1,6 +1,12 @@
 package com.jocmp.capy.common
 
 import com.jocmp.capy.Feed
+import java.text.Collator
+import java.util.Locale
 
 fun List<Feed>.sortedByTitle() =
-    sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it.title })
+    sortedWith(compareBy(Collator.getInstance(Locale.getDefault()).apply {
+        strength = Collator.PRIMARY
+    }) {
+        it.title
+    })
