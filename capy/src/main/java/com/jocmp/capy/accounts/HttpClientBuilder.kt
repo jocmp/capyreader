1db8cf42e0e9ea8e0a524ade064f4217a67b25c0
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-07-06 20:27:32 -0400

    Bump HTTP timeout from 10 seconds to 30 seconds (#1318)

--- capy/src/main/java/com/jocmp/capy/accounts/HttpClientBuilder.kt
+++ capy/src/main/java/com/jocmp/capy/accounts/HttpClientBuilder.kt
@@ -5,10 +5,16 @@ import okhttp3.Cache
 import okhttp3.OkHttpClient
 import java.io.File
 import java.net.URI
+import java.util.concurrent.TimeUnit
+
+private const val TIMEOUT_SECONDS = 30L
 
 fun httpClientBuilder(cachePath: URI) = OkHttpClient
     .Builder()
     .addInterceptor(UserAgentInterceptor())
+    .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+    .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+    .writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
     .cache(
         Cache(
             directory = File(File(cachePath), "http_cache"),
