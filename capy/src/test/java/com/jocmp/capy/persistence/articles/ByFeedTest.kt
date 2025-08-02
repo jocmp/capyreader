c2e0e3008b33d0657dcabfd915e75eb64e362d34
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-08-01 22:10:24 -0500

    Truncate summary in list
    Cap summaries to 250 characters. Two lines of text is ~100.

--- capy/src/test/java/com/jocmp/capy/persistence/articles/ByFeedTest.kt
+++ capy/src/test/java/com/jocmp/capy/persistence/articles/ByFeedTest.kt
@@ -33,6 +33,29 @@ class ByFeedTest {
     }
 
     @Test
+    fun all_summaryTruncation() = runTest {
+        val summary = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent turpis nisi, hendrerit in lobortis ac, cursus quis odio. Etiam gravida lacinia sodales. Ut sodales orci a auctor blandit. Pellentesque ultrices faucibus magna sed rhoncus. Praesent vulputate finibus auctor. Sed a neque nec odio imperdiet finibus vitae ac ipsum. Cras mollis tincidunt suscipit. Donec quis dui eget sem ultrices faucibus eget efficitur lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        """.trimIndent()
+        val article = articleFixture.create(summary = summary)
+
+        val expectedSummary = summary.take(250)
+
+        val articles = ByFeed(database)
+            .all(
+                feedIDs = listOf(article.feedID),
+                status = ArticleStatus.ALL,
+                unreadSort = UnreadSortOrder.NEWEST_FIRST,
+                since = OffsetDateTime.now().minusDays(7),
+                query = null,
+                limit = 1,
+                offset = 0,
+            ).executeAsList()
+
+        assertEquals(expected = expectedSummary, actual = articles[0].summary)
+    }
+
+    @Test
     fun findPages() = runTest {
         val feed = feedFixture.create(feedURL = "https://example.com/${RandomUUID.generate()}")
         val publishedAt = TimeHelpers.nowUTC()
