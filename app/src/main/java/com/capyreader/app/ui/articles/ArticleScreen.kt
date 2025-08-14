d2155af2a4b497c9502fa770e82ca0d67530b509
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-08-13 21:15:57 -0500

    Only refresh on start when "On Start" is selected
    Skips refresh when "Manually" or a periodic interval is set.

--- app/src/main/java/com/capyreader/app/ui/articles/ArticleScreen.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/ArticleScreen.kt
@@ -162,7 +162,7 @@ fun ArticleScreen(
     ) {
         val openNextFeedOnReadAll = afterReadAll == AfterReadAllBehavior.OPEN_NEXT_FEED
 
-        val skipInitialRefresh = refreshInterval == RefreshInterval.MANUALLY_ONLY
+        val skipInitialRefresh = refreshInterval != RefreshInterval.ON_START
 
         val (isRefreshInitialized, setRefreshInitialized) = rememberSaveable {
             mutableStateOf(skipInitialRefresh)
