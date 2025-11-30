5446c47aecd5c02180ae46a4d4de8b2c95ec8206
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-11-30 01:01:08 -0600

    Bring back key for scroll state

--- app/src/main/java/com/capyreader/app/ui/articles/detail/ArticleView.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/detail/ArticleView.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -168,10 +169,12 @@ fun ArticleView(
                         selectNext()
                     },
                 ) {
-                    ArticleReader(
-                        article = article,
-                        onSelectMedia = onSelectMedia,
-                    )
+                    key(article.id) {
+                        ArticleReader(
+                            article = article,
+                            onSelectMedia = onSelectMedia,
+                        )
+                    }
                 }
             }
         },
