66da2c7ec55fb0d2906d48c0514b43d267bc468e
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2026-03-14 17:29:02 -0500

    fix: Add UTF-8 content type for bookmarks export

--- capy/src/main/java/com/jocmp/capy/StarredBookmarksFile.kt
+++ capy/src/main/java/com/jocmp/capy/StarredBookmarksFile.kt
@@ -14,6 +14,7 @@ class StarredBookmarksFile(
         buildString {
             appendLine("<!DOCTYPE NETSCAPE-Bookmark-file-1>")
             appendLine("<!-- This is an automatically generated file. -->")
+            appendLine("<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\">")
             appendLine("<TITLE>Starred Articles</TITLE>")
             appendLine("<H1>Starred Articles</H1>")
             appendLine("<DL><p>")
