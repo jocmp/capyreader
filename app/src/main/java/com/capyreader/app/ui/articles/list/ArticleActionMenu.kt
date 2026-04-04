78e142838e212686493973b1383f812b6a77b9a5
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2026-04-03 23:19:49 -0400

    Use build-in icons (#1994)

--- app/src/main/java/com/capyreader/app/ui/articles/list/ArticleActionMenu.kt
+++ app/src/main/java/com/capyreader/app/ui/articles/list/ArticleActionMenu.kt
@@ -3,6 +3,8 @@ package com.capyreader.app.ui.articles.list
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Label
 import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.material.icons.rounded.ArrowDownward
+import androidx.compose.material.icons.rounded.ArrowUpward
 import androidx.compose.material.icons.rounded.ContentCopy
 import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.DropdownMenu
@@ -11,7 +13,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -62,7 +63,7 @@ fun ArticleActionMenu(
                 DropdownMenuItem(
                     leadingIcon = {
                         Icon(
-                            painterResource(R.drawable.icon_rounded_arrow_upward),
+                            Icons.Rounded.ArrowUpward,
                             contentDescription = null
                         )
                     },
@@ -73,7 +74,7 @@ fun ArticleActionMenu(
             DropdownMenuItem(
                 leadingIcon = {
                     Icon(
-                        painterResource(R.drawable.icon_rounded_arrow_downward),
+                        Icons.Rounded.ArrowDownward,
                         contentDescription = null
                     )
                 },
@@ -197,7 +198,7 @@ private fun ToggleActionMenuItem(
     DropdownMenuItem(
         leadingIcon = {
             Icon(
-                painterResource(action.icon),
+                action.icon,
                 contentDescription = null
             )
         },
