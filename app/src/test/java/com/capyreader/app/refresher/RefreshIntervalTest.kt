81eb3623a75684d4dc8ad2e722a123c308b6cf9f
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2026-04-26 21:19:18 -0500

    Remove 'On start' refresh interval option

--- app/src/test/java/com/capyreader/app/refresher/RefreshIntervalTest.kt
+++ app/src/test/java/com/capyreader/app/refresher/RefreshIntervalTest.kt
@@ -2,7 +2,6 @@ package com.capyreader.app.refresher
 
 import com.capyreader.app.refresher.RefreshInterval.EVERY_12_HOURS
 import com.capyreader.app.refresher.RefreshInterval.MANUALLY_ONLY
-import com.capyreader.app.refresher.RefreshInterval.ON_START
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -11,11 +10,6 @@ class RefreshIntervalTest {
     @Test
     fun isPeriodic_manual() {
        assertFalse(MANUALLY_ONLY.isPeriodic)
-    }
-
-    @Test
-    fun isPeriodic_onStart() {
-        assertFalse(ON_START.isPeriodic)
     }
 
     @Test
